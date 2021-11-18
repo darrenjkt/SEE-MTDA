18a0e7e4efeecbc093a2f994b982111cb4ff4d69
Darren <darrenjktsai@gmail.com>
2021-11-18 17:34:10 +1100

    working repo

--- docker/run.sh
+++ docker/run.sh
@@ -2,13 +2,13 @@
 
 # Local paths
 #DATA_PATH="/media/darren/dcsky/datasets"
-#CODE_PATH="/home/darren/Dropbox/code/perception/projects/ST3D"
+#CODE_PATH="/home/darren/Dropbox/code/perception/projects/SEE-MTDA"
 
 # Monolith paths
 DATA_PATH="/mnt/big-data/darren/data"
-CODE_PATH="/mnt/big-data/darren/code/ST3D"
+CODE_PATH="/mnt/big-data/darren/code/SEE-MTDA"
 
-GPU_NAME="g2080ti"
+GPU_NAME="g1080ti"
 
 declare -A GPUS
 GPUS["g2080ti"]="0"
@@ -16,20 +16,18 @@ GPUS["g1080ti"]="1"
 GPUS["g1080"]="2"
 
 GPU_ID=${GPUS[$GPU_NAME]}
-NAME="darrenst3d_${GPU_NAME}"
+NAME="darren_see-mtda_${GPU_NAME}"
 echo "Starting docker container $NAME - gpu:$GPU_NAME, ID:${GPUS[$GPU_NAME]}"
 
 ENVS="  --env=NVIDIA_VISIBLE_DEVICES=$GPU_ID
         --env=CUDA_VISIBLE_DEVICES=$GPU_ID
         --env=NVIDIA_DRIVER_CAPABILITIES=all"
 
-VOLUMES="       --volume=$DATA_PATH/kitti/3d_object_detection:/ST3D/data/kitti
-                --volume=$DATA_PATH/nuscenes:/ST3D/data/nuscenes
-                --volume=$DATA_PATH/waymo:/ST3D/data/waymo
-                --volume=$DATA_PATH/baraja:/ST3D/data/baraja
-                --volume=$CODE_PATH/pcdet:/ST3D/pcdet
-                --volume=$CODE_PATH/tools:/ST3D/tools
-                --volume=$CODE_PATH/output:/ST3D/output"
+VOLUMES="       --volume=$DATA_PATH/kitti/3d_object_detection:/SEE-MTDA/data/kitti
+                --volume=$DATA_PATH/nuscenes:/SEE-MTDA/data/nuscenes
+                --volume=$DATA_PATH/waymo:/SEE-MTDA/data/waymo
+                --volume=$DATA_PATH/baraja:/SEE-MTDA/data/baraja
+                --volume=$CODE_PATH:/SEE-MTDA"
 
 VISUAL="        --env=DISPLAY
                 --env=QT_X11_NO_MITSHM=1
@@ -46,6 +44,8 @@ docker  run -d -it --rm \
         --gpus $GPU_ID \
         --privileged \
         --net=host \
-        --workdir=/ST3D \
-	darrenjkt/st3d:v0.3.0
+        --workdir=/SEE-MTDA \
+        anonymoustofu/see-mtda:see-v1.0
+        # darrenjkt/mmdetection:v2.14.0-1
+	
 
--- see/mmdetection/tools/see_masks/prepare_kitti_masks.sh
+++ see/mmdetection/tools/see_masks/prepare_kitti_masks.sh
@@ -6,10 +6,10 @@ for cam in "${cameras[@]}"
 do 
 	echo "Getting masks for $cam"
 
-	python /mmdetection/tools/see_masks/create_masks.py \
-	--config "/mmdetection/configs/htc/htc_x101_64x4d_fpn_dconv_c3-c5_mstrain_400_1400_16x1_20e_coco.py" \
-	--checkpoint "/mmdetection/checkpoints/htc_x101_64x4d_fpn_dconv_c3-c5_mstrain_400_1400_16x1_20e_coco_20200312-946fd751.pth" \
-	--data_dir "/mmdetection/data/kitti/training/$cam" \
-	--output_json "/mmdetection/data/kitti/training/masks/htc/$cam.json"
+	python /SEE-MTDA/see/mmdetection/tools/see_masks/create_masks.py \
+	--config "/SEE-MTDA/see/mmdetection/configs/htc/htc_x101_64x4d_fpn_dconv_c3-c5_mstrain_400_1400_16x1_20e_coco.py" \
+	--checkpoint "/SEE-MTDA/see/mmdetection/checkpoints/htc_x101_64x4d_fpn_dconv_c3-c5_mstrain_400_1400_16x1_20e_coco_20200312-946fd751.pth" \
+	--data_dir "/SEE-MTDA/data/kitti/training/$cam" \
+	--output_json "/SEE-MTDA/data/kitti/training/masks/htc/$cam.json"
 
 done
